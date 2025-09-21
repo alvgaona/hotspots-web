ca9d0ed69c48afeb074eb9a2c4bfd4c29f6fdb8a
Alvaro Gaona <alvgaona@gmail.com>
2025-09-21 20:34:37 +0200

    feat: set the main theme for the website (#1)

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+	adapter: netlify(),
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
